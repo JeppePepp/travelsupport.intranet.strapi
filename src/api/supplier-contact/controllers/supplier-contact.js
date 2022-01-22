3a548be71c60c244a0d5edc949a5a01c91636890
Jesper Pettersson <carl.jesper.pettersson@gmail.com>
2022-01-22 01:52:49 +0100

    fix

---
+++ src/api/supplier-contact/controllers/supplier-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  supplier-contact controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::supplier-contact.supplier-contact');
---
+++ src/api/supplier-contact/routes/supplier-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * supplier-contact router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::supplier-contact.supplier-contact');
