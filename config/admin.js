ed7736faee6c369b7b96c0956737bb3912ac95fd
JeppePepp <carl.jesper.pettersson@gmail.com>
2022-06-22 10:29:10 +0200

    agent type

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '253b88de7be2cd5d5b638e44a4206a14'),
+    secret: env("ADMIN_JWT_SECRET", "253b88de7be2cd5d5b638e44a4206a14"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
   },
 });
